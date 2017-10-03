1ee46ff0542b10fd04cd743cfd9886f83fd04811
chri-roch12 <chris.prochnow@gmail.com>
2017-10-03 14:51:19 -0700

    Setup initial testing suite Jest Enzyme PropTypes

--- src/App.js
+++ src/App.js
@@ -263,3 +263,9 @@ class App extends Component {
 }
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import { configure, shallow } from 'enzyme';
+import App, { Search, Button, Table } from './App';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+// Search component tests
+
+describe('Search', () => {
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+  })
+
+  test('snapshots', () => {
+    const component =renderer.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+// Button component tests
+
+describe('Button', () => {
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Give Me More</Button>, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Button>Give Me More</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+// TABLE component tests
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
+
 });
